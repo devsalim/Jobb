86bf569477d2962c2aa83bd819dcdcb3a0de1407
devvicky <vickyvishalcse@gmail.com>
2015-09-17 23:12:08 +0530

    updated postjob migration

--- database/migrations/2015_09_13_171750_create_postjobs_table.php
+++ database/migrations/2015_09_13_171750_create_postjobs_table.php
@@ -15,20 +15,28 @@ class CreatePostjobsTable extends Migration {
 		Schema::create('postjobs', function(Blueprint $table)
 		{
 			$table->increments('id');
-			$table->string('post_type');
-            $table->string('post_title');
-            $table->string('post_duration');
-            $table->string('post_compname');
-            $table->string('linked_skill');
-            $table->string('email_id');
-            $table->string('phone');
-            $table->string('operating_since');
-            $table->string('prof_category');
-            $table->string('alt_emailid');
-            $table->string('alt_phone');
-            $table->string('role');
-            $table->string('state');
-            $table->string('city');
+			$table->string('post_type')->nullable();
+			$table->string('individual_id')->nullable();
+			$table->string('corporate_id')->nullable();
+            $table->string('post_title')->nullable();
+            $table->string('post_duration')->nullable();
+            $table->string('post_compname')->nullable();
+            $table->string('linked_skill')->nullable();
+            $table->string('email_id')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('min_exp')->nullable();
+            $table->string('max_exp')->nullable();
+            $table->string('min_sal')->nullable();
+            $table->string('max_sal')->nullable();
+            $table->string('prof_category')->nullable();
+            $table->string('alt_emailid')->nullable();
+            $table->string('alt_phone')->nullable();
+            $table->string('job_detail')->nullable();
+            $table->string('role')->nullable();
+            $table->string('state')->nullable();
+            $table->string('city')->nullable();
+            $table->foreign('individual_id')->references('id')->on('indusers');
+            $table->foreign('corporate_id')->references('id')->on('corpusers');
 			$table->timestamps();
 		});
 	}
