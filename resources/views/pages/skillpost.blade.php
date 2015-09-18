cfb42b799d4bfa10d056178712b9367411be5f98
devvicky <vickyvishalcse@gmail.com>
2015-09-18 13:35:09 +0530

    skillpost page added and changes

--- resources/views/pages/skillpost.blade.php
+++ resources/views/pages/skillpost.blade.php
@@ -1,23 +1,22 @@
-
-
-<div class="row">
-
-
-
-
-
-
-
-
-<div class="col-md-12">
-								<div class="portlet light">
-									<div class="portlet-title tabbable-line">
-										<label style="font-size: 19px;text-align: center;margin: auto;width: 75%;border-bottom:2px solid darkred;">Do you know about any job opening?<br>Post Job information for FREE!!</label>
-										
-									</div>
-									<div class="portlet-body form" style="width: 75%;">
-										
-													<div class="row-md-6">
+<div class="portlet light">
+	<div class="portlet-title">									
+		<label style="font-size: 19px;text-align: center;margin: auto;width: 75%;border-bottom:2px solid darkred;">Do you like to Post your Skill?<br>Post Skill for FREE!!</label>									
+		<div class="tools">
+			<a href="" class="collapse">
+			</a>
+			<a href="#portlet-config" data-toggle="modal" class="config">
+			</a>
+			<a href="" class="reload">
+			</a>
+			<a href="" class="remove">
+			</a>
+		</div>
+	</div>
+	<div class="portlet-body form" style="width: 70%;">
+											<!-- BEGIN FORM-->
+											<form action="#" class="horizontal-form">
+												<div class="form-body">
+													<div class="row">
 														<div class="col-md-12">
 															<div class="form-group">
 															
@@ -33,24 +32,21 @@
 															</div>
 														</div>
 													</div>
-													<div class="row-md-3"></div>
-													
-													
-												
+													<!--/row-->
 													<div class="row-md-2"></div>
-													<div class="row-md-10">
+													<div class="row">
 														<div class="col-md-6">
 															<div class="form-group">
 																<label>Job Category</label>
 																<div class="input-group">
-													<span class="input-group-addon">
-													<i class="fa fa-university " style="color:darkcyan;"></i>
-													</span>
-													<select class="form-control" >
-														<option value="AL">Alabama</option>
-														<option value="WY">Wyoming</option>
-													</select>
-												</div>
+																<span class="input-group-addon">
+																<i class="fa fa-university " style="color:darkcyan;"></i>
+																</span>
+																<select class="form-control" >
+																	<option value="AL">Alabama</option>
+																	<option value="WY">Wyoming</option>
+																</select>
+															</div>
 															</div>
 														</div>
 														<!--/span-->
@@ -58,20 +54,20 @@
 															<div class="form-group">
 																<label>Job Role</label>
 																<div class="input-group">
-													<span class="input-group-addon">
-													<i class="fa fa-university" style="color:darkcyan;"></i>
-													</span>
-													<select class="form-control" >
-														<option value="AL">Alabama</option>
-														<option value="WY">Wyoming</option>
-													</select>
-												</div>
+																	<span class="input-group-addon">
+																	<i class="fa fa-university" style="color:darkcyan;"></i>
+																	</span>
+																	<select class="form-control" >
+																		<option value="AL">Alabama</option>
+																		<option value="WY">Wyoming</option>
+																	</select>
+																</div>
 															</div>
 														</div>
 														<!--/span-->
 													</div>
 													<div class="row-md-2"></div>
-													<div class="row-md-10">
+													<div class="row">
 														<div class="col-md-6">
 															<div class="form-group">
 																<label>City</label>
@@ -89,29 +85,29 @@
 															<div class="form-group">
 																<label>State</label>
 																<div class="input-group">
-													<span class="input-group-addon">
-													<i class="fa fa-map-marker" style="color:darkcyan;"></i>
-													</span>
-													<select class="form-control" >
-														<option value="AL">Alabama</option>
-														<option value="WY">Wyoming</option>
-													</select>
-												</div>
+																	<span class="input-group-addon">
+																	<i class="fa fa-map-marker" style="color:darkcyan;"></i>
+																	</span>
+																	<select class="form-control" >
+																		<option value="AL">Alabama</option>
+																		<option value="WY">Wyoming</option>
+																	</select>
+																</div>
 															</div>
 														</div>
 														<!--/span-->
 													</div>
-													
+													<!--/row-->
 													<div class="row-md-2"></div>
-													<div class="row-md-10">
+													<div class="row">
 														<div class="col-md-6">
 															<div class="form-group">
 																
 																
 																<label class=" control-label">Experience (in Years)</label>
-											<div class="">
-												<input id="range_1" type="text" name="range_1" value=""/>
-											</div>
+																<div class="">
+																	<input id="range_1" type="text" name="range_1" value=""/>
+																</div>
 															</div>
 														</div>
 														<!--/span-->
@@ -120,44 +116,24 @@
 																
 																
 																<label class=" control-label"><input type="checkbox">&nbsp;Salary (<i class="fa fa-rupee (alias)"></i>/Month)</label>
-											<div class="">
-												<input id="range_7" type="text" name="range_1" value=""/>
-											</div>
+																<div class="">
+																	<input id="range_7" type="text" name="range_1" value=""/>
+																</div>
 															</div>
 														</div>
 														<!--/span-->
 													</div>
-													<script type="text/javascript">
-													function ShowHide(chk,txt)
-
-    {          
-
-        if(document.getElementById(chk).checked)  
-
-            document.getElementById(txt).style.display='';
-
-        else
-
-            document.getElementById(txt).style.display='none';  
-    }
-
-
-													</script>
-													<div class="row-md-10">
-													<div class="form-group">
-													<label style="
-    padding-left: 15px;
-">Job Details</label>
-											<div class="col-md-12" style="
-    padding-bottom: 10px;
-">
-												<textarea class="wysihtml5 form-control" rows="6"></textarea>
-											</div>
-											</div></div>
+													<!--/row-->
+													<div class="row">
+														<div class="form-group">
+															<label style="padding-left: 15px;">Job Details</label>
+															<div class="col-md-12" style=" padding-bottom: 10px;">
+															<textarea class="wysihtml5 form-control" rows="6"></textarea>
+															</div>
+														</div>
+													</div>
 													
-													
-													<br><br><br>
-													<div class="row-md-10">
+													<div class="row">
 														<div class="col-md-6">
 															<div class="form-group">
 																<label >Key Skills</label>
@@ -184,8 +160,8 @@
 														</div>
 														<!--/span-->
 													</div>
-													<div class="row-md-2"></div>
-													<div class="row-md-10">
+													<!--/row-->
+													<div class="row">
 														<div class="col-md-6">
 															<div class="form-group">
 																<label>Email Id (Registered)</label>
@@ -200,7 +176,7 @@
 														<!--/span-->
 														<div class="col-md-6">
 															<div class="form-group">
-																<label>Email Id</label>
+																<label>Alternate Email Id (Optional)</label>
 																<div class="input-group">
 																<span class="input-group-addon">
 																<i class="icon-envelope" style="color:darkcyan;"></i>
@@ -211,8 +187,7 @@
 														</div>
 														<!--/span-->
 													</div>
-													<div class="row-md-2"></div>
-													<div class="row-md-10">
+													<div class="row">
 														<div class="col-md-6">
 															<div class="form-group">
 																<label>Phone No (Registered)</label>
@@ -221,8 +196,6 @@
 																<i class="icon-call-end" style="color:darkcyan;"></i>
 																</span>
 																<input type="phone" class="form-control" placeholder="">
-																
-		</span>
 																</div>
 															</div>
 														</div>
@@ -240,48 +213,32 @@
 														</div>
 														<!--/span-->
 													</div>
-													<div class="form-actions ">
+												</div>
+												<div class="form-actions">
 													<button type="button" class="btn default">Cancel</button>
 													<button type="submit" class="btn blue"><i class="fa fa-check"></i> Save</button>
 												</div>
-												</div>
-											
-											</div>
-										
-										
-							
+											</form>
+											<!-- END FORM-->
+										</div>
+									</div>
 
-
-
-
-
-
-
-
-
-
-</div>
-
-
-									
-	
-	
 <script>
 jQuery(document).ready(function() {       
    // initiate layout and plugins
-   Metronic.init(); // init metronic core components
-Layout.init(); // init current layout
-Demo.init(); // init demo features
-   ComponentsEditors.init();
-   ComponentsIonSliders.init();
+   	Metronic.init(); // init metronic core components
+	Layout.init(); // init current layout
+	Demo.init(); // init demo features
+    ComponentsIonSliders.init();
 });   
 </script>
 <script>
-jQuery(document).ready(function() {
-// initiate layout and plugins
-Metronic.init(); // init metronic core components
-Layout.init(); // init current layout
-Demo.init(); // init demo features
-FormEditable.init();
-});
+jQuery(document).ready(function() {       
+   // initiate layout and plugins
+    Metronic.init(); // init metronic core components
+	Layout.init(); // init current layout
+	Demo.init(); // init demo features
+    ComponentsEditors.init();
+
+});   
 </script>
