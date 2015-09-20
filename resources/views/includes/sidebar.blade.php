e87238f3653f758d4e0cecf8ba8927150c8040a2
Salim <salimsudden@gmail.com>
2015-09-20 16:36:45 +0530

    minor

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -1,4 +1,4 @@
-BEGIN SIDEBAR1 -->
+<!-- BEGIN SIDEBAR1 -->
 <div class="page-sidebar-wrapper">
   <!-- DOC: Set data-auto-scroll="false" to disable the sidebar from auto scrolling/focusing -->
   <!-- DOC: Change data-auto-speed="200" to adjust the sub menu slide up/down speed -->
