df6294d1f9d232126d23bb76be2d7fe40c316e9b
Salim <salimsudden@gmail.com>
2015-10-24 00:57:43 +0530

    Home Connection, follow update

---
+++ database/migrations/2015_10_22_063837_create_follows_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFollowsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('follows', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('corporate_id')->unsigned();
+			$table->integer('individual_id')->unsigned();
+			$table->foreign('corporate_id')->references('id')->on('corpusers');
+			$table->foreign('individual_id')->references('id')->on('indusers');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('follows');
+	}
+
+}
