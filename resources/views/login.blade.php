f236fb4195023229cd4b88bfb56087d564cc13a1
devvicky <vickyvishalcse@gmail.com>
2015-10-26 22:35:34 +0530

    login page logo change

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -108,7 +108,7 @@ input:focus:-ms-input-placeholder { color:transparent !important; } /* IE 10+ */
 <div class="logo">
 <picture>
     <source srcset="assets/admin/layout/img/logo-big.png">
-    <img srcset="assets/admin/layout/img/logo-big.png" alt="My default image"  style="max-width:295px;margin-top: -10px;margin-bottom: -13px;">
+    <img srcset="assets/new_big_logo.png" alt="My default image"  style="max-width:295px;margin-top: -10px;margin-bottom: -13px;">
 </picture>
 </div>
 
