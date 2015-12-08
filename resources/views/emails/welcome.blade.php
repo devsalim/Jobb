5d0ac9d48cf9c2d55b082e41c461646275b1548a
Salim <salimsudden@gmail.com>
2015-12-08 23:55:02 +0530

    Welcome email template change

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -10,7 +10,9 @@
 <p>We'd like to personally welcome you to the Jobtip. Thank you for registering!</p>
 
 <p>Click the button to activate your account.</p><br/>
-<a href="http://www.jobtip.in/verify/{{$vcode}}">Activate</a>
+<a href="http://www.jobtip.in/verify/{{$vcode}}">Activate</a><br/>
+
+Your verification code : {{$vcode}}
 
 </body>
 </html>
